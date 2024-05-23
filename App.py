2fe1ea8a9016f7becf363565e0cc4de0596430d2
nishida675 <hoge93141@gmail.com>
2024-05-24 01:03:30 +0900

    5/24

--- App.py
+++ App.py
@@ -1,1 +1,20 @@
-#streamlitで書く
+import streamlit as st
+from pytube import YouTube
+from YoutubeDL import youtubeDL
+
+st.title('Youtube ダウンロード')
+
+with st.form(key="name-form"):
+ inputText = st.text_input("youtubeリンク", key="link")
+ submit_button = st.form_submit_button(label="探す")
+
+ if submit_button and inputText:
+    downloader = youtubeDL()
+    yt = downloader.checkYoutube(inputText)
+    if yt:
+     st.write(f"動画のタイトル: {yt['title']}")
+     st.image(yt['thumbnail_url']) 
+    else:
+      st.write("動画が見つかりません。")  
+ 
+
--- YoutubeDL.py
+++ YoutubeDL.py
@@ -1,19 +1,33 @@
 from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
 
+class youtubeDL:
+    def checkYoutube(self, video_url):
+      try:
+       yt = YouTube(video_url)
+       return {
+                'title': yt.title,
+                'thumbnail_url': yt.thumbnail_url,
+                'yt_object': yt
+            }
+      except VideoUnavailable:
+            print("指定された動画は利用できません。")
+      except Exception as e:
+            print(f"エラーが発生しました: {e}")
 # ダウンロードしたい動画のURLを設定
-video_url = 'https://www.youtube.com/watch?v=kj8kpQhiZpM'
+#video_url = 'https://www.youtube.com/watch?v=kj8kpQhiZpM'
 
 # YouTubeオブジェクトを作成
-yt = YouTube(video_url)
+#yt = YouTube(video_url)
 
 # オーディオストリームをフィルタリングして取得
-audio_streams = yt.streams.filter(only_audio=True)
+#audio_streams = yt.streams.filter(only_audio=True)
 #audio_streams = yt.streams.filter(file_extension='mp4').all()
 
 # 最初のオーディオストリームを選択
-audio_stream = audio_streams.first()
+#audio_stream = audio_streams.first()
 
 # オーディオストリームをダウンロード
-audio_stream.download()
+#audio_stream.download()
 
 print("ダウンロードが完了しました。")
